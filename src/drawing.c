2d814948228627e094f5eeb8a6000ad53beb64de
JSaurusRex <jessetonckens@gmail.com>
2022-05-09 17:15:17 +0200

    added vignette to loading screen

--- src/drawing.c
+++ src/drawing.c
@@ -403,6 +403,8 @@ void drawLoadScreen()
 	DrawTextureTiled(_menuBackground, (Rectangle){.x=GetTime()*50, .y=GetTime()*50, .height = _background.height, .width= _background.width},
 		(Rectangle){.x=0, .y=0, .height = GetScreenHeight(), .width= GetScreenWidth()}, (Vector2){.x=0, .y=0}, 0, 0.2, ColorAlpha(WHITE, _loadingFade));
 	DrawRing((Vector2){.x=GetScreenWidth()/2, .y=GetScreenHeight()/2}, GetScreenWidth()*0.1, GetScreenWidth()*0.15, angle, angle+170, 50, ColorAlpha(WHITE, _loadingFade));
+	if(_loadingFade== 1)
+		drawVignette();
 }
 
 void drawButtonNoSprite(Rectangle rect, char * text, float fontScale)
